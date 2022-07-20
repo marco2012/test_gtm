cc821b845e4da8ed5ea2447114ffaba8bc94f636
Marco <marco.fox.marco@gmail.com>
2022-07-20 19:02:23 +0200

    implemented gtm

---
+++ main.js
@@ -0,0 +1,42 @@
+output();
+
+function output() {
+    var hDL = syntaxHighlight(JSON.stringify(dataLayer, undefined, 4));
+    document.getElementById("DL").innerHTML = hDL;
+}
+
+function syntaxHighlight(json) {
+    json = json
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+    return json.replace(
+        /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+        function (match) {
+            var cls = "number";
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = "key";
+                } else {
+                    cls = "string";
+                }
+            } else if (/true|false/.test(match)) {
+                cls = "boolean";
+            } else if (/null/.test(match)) {
+                cls = "null";
+            }
+            return '<span class="' + cls + '">' + match + "</span>";
+        }
+    );
+}
+
+function btnClick(label) {
+    dataLayer.push({
+        event: "click",
+        event_info: {
+            event_category: "button",
+            event_label: label,
+        },
+    });
+    output();
+}
